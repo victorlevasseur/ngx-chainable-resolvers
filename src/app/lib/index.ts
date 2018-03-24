0cc73d9a96adc2830bfb0467693741cd49c27c7d
Victor Levasseur <victorlevasseur52@gmail.com>
2018-03-25 00:10:34 +0100

    Configure ng-packagr

---
+++ src/app/lib/index.ts
@@ -0,0 +1,5 @@
+export * from './chainable-resolver';
+export * from './chainable-resolver-factory';
+export * from './builders/chain-bound-resolver-builder';
+export * from './builders/chain-bound-resolver-builder-interface';
+export * from './builders/chain-bound-root-builder';
--- src/app/lib/private/chain-bound-root.ts
+++ src/app/lib/private/chain-bound-root.ts
@@ -1,6 +1,7 @@
 import { ChainBoundResolverInterface } from './chain-bound-resolver-interface';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 export class ChainBoundRoot implements ChainBoundResolverInterface<{}> {
 
